18055a78b8baf23ccf2b3429a358af8d5740ba45
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-05-08 12:57:18 +0530

    Add files via upload

---
+++ app58.js
@@ -0,0 +1,28 @@
+function hello() {
+    console.log("hello");
+}
+
+hello();
+
+function printName() {
+    console.log("Shreyas");
+}
+
+function print1to5() {
+    for(let i = 1; i<=5; i++) {
+        console.log(i)
+    }
+}
+
+function isAdult() {
+    let age = 5;
+    if(age >= 18) {
+        console.log("adult");
+    }
+    else {
+        console.log("not adult");
+    }
+}
+
+isAdult();
+print1to5();
